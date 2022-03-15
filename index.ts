14e6a23db73b8aa935138703e4cb588ead33b65a
Katy DeCorah <katydecorah@gmail.com>
2022-03-15 19:41:32 -0400

    Clean up

--- index.ts
+++ index.ts
@@ -25,16 +25,22 @@ const checkAltText = lintRule(
 
     const aggregate = (node: textNode, ancestors: textNode[]) => {
       imageIsLink = ancestors.filter((a) => a.type === "link").length > 0;
+
       const { alt } = node;
+
       if (alt) hasAltText = true;
+
       if (!alt && !imageIsLink) {
         const suggestion = altText(undefined, options);
         if (suggestion) file.message(suggestion, node);
       }
+
       if (!alt && imageIsLink && options["image-is-link"] !== false) {
         file.message(imageLink.check(), node);
       }
+
       if (!alt) return;
+
       if (!textToNodes[alt]) {
         textToNodes[alt] = [];
       }
@@ -46,6 +52,7 @@ const checkAltText = lintRule(
     for (const alt of Object.keys(textToNodes)) {
       const nodes = textToNodes[alt];
       if (!nodes) return;
+
       for (const node of nodes) {
         const suggestion = altText(node.alt, options);
         if (suggestion) file.message(suggestion, node);
