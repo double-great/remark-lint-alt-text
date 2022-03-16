c17de4b974d9576a47fe79b24d24e2169c38e95e
Katy DeCorah <katydecorah@gmail.com>
2022-03-15 20:00:40 -0400

    Clean up

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -7,67 +7,9 @@ const processMarkdown = (markdown: string, opts?: Config) => {
   return remark().use(plugin, opts).process(markdown);
 };
 
-it("End in period", async () => {
-  const lint = await processMarkdown(dedent`
-      # Title of my site
-
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
-      ![alt texter](https://site.com/image.png)
-
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    `);
-  expect(lint.messages.length).toEqual(1);
-  expect(lint.messages[0].reason).toMatchInlineSnapshot(
-    `"Alt text should end with punctuation (https://tinyurl.com/y5krn3ny)."`
-  );
-});
-
-it("End in period, disabled", async () => {
-  const lint = await processMarkdown(
-    dedent`
-      # Title of my site
-
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
-      ![alt texter](https://site.com/image.png)
-
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    `,
-    { "end-with-punctuation": false }
-  );
-  expect(lint.messages.length).toEqual(0);
-});
-
-it("Image is a link, has alt text", async () => {
-  const lint = await processMarkdown(dedent`
-      [![Puppies.](https://site.com/image.png)](https://website.org)
-    `);
-  expect(lint.messages.length).toEqual(0);
-});
-
-it("Image is a link, should have alt text", async () => {
-  const lint = await processMarkdown(dedent`
-      [![](https://site.com/image.png)](https://website.org)
-    `);
-  expect(lint.messages.length).toEqual(1);
-  expect(lint.messages[0].reason).toMatchInlineSnapshot(
-    `"Images inside a link tag require alt text that describes the purpose of the link (https://tinyurl.com/yxhq2k5w)."`
-  );
-});
-
-it("Image is a link, disabled", async () => {
-  const lint = await processMarkdown(
-    dedent`
-      [![](https://site.com/image.png)](https://website.org)
-    `,
-    { "image-is-link": false }
-  );
-  expect(lint.messages.length).toEqual(0);
-});
-
-it("No warnings", async () => {
-  const lint = await processMarkdown(dedent`
+describe("No warnings", () => {
+  it("passes", async () => {
+    const lint = await processMarkdown(dedent`
       # Title of my site
 
 
@@ -78,33 +20,99 @@ it("No warnings", async () => {
       ![A child holding a photograph.](https://site.com/kiddo.png)
 
     `);
-  expect(lint.messages.length).toEqual(0);
+    expect(lint.messages.length).toEqual(0);
+  });
 });
 
-it("Empty alt text", async () => {
-  const lint = await processMarkdown(dedent`
+describe("End in period", () => {
+  it("throws", async () => {
+    const lint = await processMarkdown(dedent`
+      # Title of my site
+
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+      ![alt texter](https://site.com/image.png)
+
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    `);
+    expect(lint.messages.length).toEqual(1);
+    expect(lint.messages[0].reason).toMatchInlineSnapshot(
+      `"Alt text should end with punctuation (https://tinyurl.com/y5krn3ny)."`
+    );
+  });
+
+  it("disabled", async () => {
+    const lint = await processMarkdown(
+      dedent`
+      # Title of my site
+
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+      ![alt texter](https://site.com/image.png)
+
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    `,
+      { "end-with-punctuation": false }
+    );
+    expect(lint.messages.length).toEqual(0);
+  });
+});
+
+describe("Image is a link", () => {
+  it("does not throw", async () => {
+    const lint = await processMarkdown(dedent`
+      [![Puppies.](https://site.com/image.png)](https://website.org)
+    `);
+    expect(lint.messages.length).toEqual(0);
+  });
+
+  it("throws", async () => {
+    const lint = await processMarkdown(dedent`
+      [![](https://site.com/image.png)](https://website.org)
+    `);
+    expect(lint.messages.length).toEqual(1);
+    expect(lint.messages[0].reason).toMatchInlineSnapshot(
+      `"Images inside a link tag require alt text that describes the purpose of the link (https://tinyurl.com/yxhq2k5w)."`
+    );
+  });
+
+  it("disabled", async () => {
+    const lint = await processMarkdown(
+      dedent`
+      [![](https://site.com/image.png)](https://website.org)
+    `,
+      { "image-is-link": false }
+    );
+    expect(lint.messages.length).toEqual(0);
+  });
+});
+
+describe("Empty alt text", () => {
+  it("throws", async () => {
+    const lint = await processMarkdown(dedent`
       # Title of my site
 
 
       ![](https://site.com/doggie.png)
 
     `);
-  expect(lint.messages.length).toEqual(1);
-  expect(lint.messages[0].reason).toMatchInlineSnapshot(
-    `"Empty alt text should only be used for decorative images (https://tinyurl.com/yxnvejgv)."`
-  );
-});
+    expect(lint.messages.length).toEqual(1);
+    expect(lint.messages[0].reason).toMatchInlineSnapshot(
+      `"Empty alt text should only be used for decorative images (https://tinyurl.com/yxnvejgv)."`
+    );
+  });
 
-it("Empty alt text, disabled", async () => {
-  const lint = await processMarkdown(
-    dedent`
+  it("disabled", async () => {
+    const lint = await processMarkdown(
+      dedent`
       # Title of my site
 
 
       ![](https://site.com/doggie.png)
 
     `,
-    { "image-is-decorative": false }
-  );
-  expect(lint.messages.length).toEqual(0);
+      { "image-is-decorative": false }
+    );
+    expect(lint.messages.length).toEqual(0);
+  });
 });
