90b4bc5dcf4ac9c16a2ab7982a618b015be50b39
Katy DeCorah <katydecorah@gmail.com>
2025-01-18 21:08:28 -0500

    Add back node position in error message (#69)
    * Add back node position in error message
    
    * Clean up

--- index.ts
+++ index.ts
@@ -9,14 +9,6 @@ import { Node } from "unist";
 import { VFile } from "vfile";
 import { TransformCallback } from "unified";
 
-type textNode = {
-  type: string;
-  title: string | null;
-  url: string;
-  alt: string | undefined;
-  position: string[];
-};
-
 type Plugin = (
   config?:
     | Label
@@ -29,17 +21,20 @@ type Plugin = (
   | undefined;
 
 const checkAltText: Plugin = lintRule(
-  "remark-lint:alt-text",
+  {
+    origin: "remark-lint:alt-text",
+    url: "https://doublegreat.dev/remark-lint-alt-text/",
+  },
   (tree: Node, file: VFile, options: Config): void => {
     options = {
       ...defaultConfig,
       ...options,
     };
-    const textToNodes: { [alt: string]: textNode[] } = {};
+    const textToNodes: { [alt: string]: TextNode[] } = {};
     let imageIsLink = false;
     let hasAltText = false;
 
-    const aggregate = (node: textNode, ancestors: textNode[]) => {
+    const aggregate = (node: TextNode, ancestors: TextNode[]) => {
       imageIsLink = ancestors.filter((a) => a.type === "link").length > 0;
 
       const { alt } = node;
@@ -48,11 +43,11 @@ const checkAltText: Plugin = lintRule(
 
       if (!alt && !imageIsLink) {
         const suggestion = altText(undefined, options);
-        if (suggestion) file.message(suggestion);
+        if (suggestion) file.message(suggestion, node.position);
       }
 
       if (!alt && imageIsLink && options["image-is-link"] !== false) {
-        file.message(imageLink.check());
+        file.message(imageLink.check(), node.position);
       }
 
       if (!alt) return;
@@ -71,13 +66,27 @@ const checkAltText: Plugin = lintRule(
 
       for (const node of nodes) {
         const suggestion = altText(node.alt, options);
-        if (suggestion) file.message(suggestion);
+        if (suggestion) file.message(suggestion, node.position);
         if (imageIsLink && !hasAltText && options["image-is-link"] !== false) {
-          file.message(imageLink.check());
+          file.message(imageLink.check(), node.position);
         }
       }
     }
   },
 );
+
+export type TextNode = {
+  type: "text" | "image" | "link";
+  title: string | null;
+  url?: string;
+  alt?: string;
+  value: string | undefined;
+  position: {
+    start: { line: number; column: number; offset?: number };
+    end: { line: number; column: number; offset?: number };
+    indent?: number[];
+  };
+  children: TextNode[];
+};
 
 export default checkAltText;
